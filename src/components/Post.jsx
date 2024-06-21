258a3b85de97e1596395961134f9303b3fe2ca04
Lucas Kepler <lucas.kepler@pbi.local>
2024-06-21 13:46:33 -0300

    finish

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img src="https://github.com/Scrylk.png" alt="" />
+          <Avatar src="https://github.com/Scrylk.png" />
           <div className={styles.authorInfo}>
             <strong>Lucas Kepler</strong>
             <span>Wed Developer</span>
@@ -16,15 +18,34 @@ export function Post() {
         </time>
       </header>
       <div className={styles.content}>
-          <p>Fala galeraa 👋 Acabei de subir mais um projeto no meu portifa. É um</p>
-          <p>projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto</p>
-          <p>é DoctorCare 🚀 <a href="#">jane.design/doctorcare</a></p>
-          <p> 
-            <a href="">#novoprojeto</a> {" "}
-            <a href="">#nlw </a>  {" "}
-            <a href="">#rocketseat</a>
-          </p>
+        <p>
+          Fala galeraa 👋 Acabei de subir mais um projeto no meu portifa. É um
+        </p>
+        <p>
+          projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto
+        </p>
+        <p>
+          é DoctorCare 🚀 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw </a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+    <div className={styles.commentList}>
+      <Comment />
+      <Comment />
+      <Comment />
+    </div>
     </article>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 import { PencilLine } from "phosphor-react";
 
@@ -10,11 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/Scrylk.png"
-          alt=""
-        />
+      <Avatar hasBorder src="https://github.com/Scrylk.png"/>
         <strong>Lucas Kepler</strong>
         <span>Web Developer</span>
       </div>
