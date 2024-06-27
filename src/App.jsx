1bb2395600675ebf616a8acf8aa3e379935409f1
Lucas Kepler <lucas.kepler@pbi.local>
2024-06-27 11:20:22 -0300

    opa

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,25 @@ import { Header } from "./components/Header";
 import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 
+
+const posts = [{
+  id: 1,
+  author: {
+    avatarUrl: "https://github.com/Scrylk.png",
+    name: "Diego Fernandes",
+    role: "CTO @Rocketseat"
+  },
+  content: [
+    { type: "paragraph", content: "Fala galeraa 👋", },
+    { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. E um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto e DoctorCare 🚀"},
+    { type: "link", content: "jane.design/doctorcare"}
+    ],
+  publishedAt: new Date("2022-05-11 08:13:58")
+  },
+];
+  
+
+
 function App() {
   return (
     <div>
@@ -13,8 +32,13 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Diego Fernandes" content="lorem ipsum dolor sit amet" />
-          <Post author="Lucas Kepler" content="fala galera" />
+          {posts.map(post => {
+           return <Post
+           key={post.id}
+           author={post.author}
+           content={post.content}
+           publishedAt={post.publishedAt} />
+          })}
         </main>
       </div>
     </div>
